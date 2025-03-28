7596cc4c19b22c9cf3009008af8eb474510d75c3
Osanim <osanimsystems@gmail.com>
2025-03-28 14:58:20 +0000

    update

--- cities.ts
+++ cities.ts
@@ -7,7 +7,7 @@ const getAll = cities()?.sort((a, b) => a.name.localeCompare(b.name));
 const sortByIsoCode = cities()?.sort((a, b) => a.iso.localeCompare(b.iso));
 
 // Get a state filtered cities
-// const getByState = (iso:string) => cities_by_state(iso)
+const getByState = (iso) => cities_by_state(iso)
 
 // Get a country filtered cities
 const getByCountry = (iso) => cities_by_country(iso)
@@ -19,6 +19,6 @@ export default {
     getAll,
     sortByIsoCode,
     getByCountry,
-    // getByState,
+    getByState,
     getByContinent
 };
