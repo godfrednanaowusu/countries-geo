b7fc9fdc197bbf9992037e46c4a642a0c4f6a17b
Osanim <osanimsystems@gmail.com>
2025-03-28 14:27:39 +0000

    update

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import Countries from './countries';
 import States from './states';
 import Cities from './cities';
 
+console.log(Continents?.getAll)
+
 export { Continents };
 export { Countries };
 export { States };
