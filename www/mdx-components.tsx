f3541205064d6bc3e89dedca4cdbfdac54d611c5
Osanim <osanimsystems@gmail.com>
2025-03-29 06:58:19 +0000

    update

--- www/mdx-components.tsx
+++ www/mdx-components.tsx
@@ -1,11 +1,12 @@
-import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs'
- 
-const docsComponents = getDocsMDXComponents()
- 
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
+
+// Get the default MDX components
+const themeComponents = getThemeComponents()
+
+// Merge components
 export function useMDXComponents(components) {
-  return {
-    ...docsComponents,
-    ...components
-    // ... your additional components
-  }
+    return {
+        ...themeComponents,
+        ...components
+    }
 }
--- www/theme.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    logo: <span>My Project</span>,
-    project: {
-      link: 'https://github.com/yourusername/your-repo',
-    },
-  };
